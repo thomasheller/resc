a2ac4ed400f483a1d7976c16cf591d2d891d31ae
Thomas Heller <thomas.m.heller@gmail.com>
2019-12-11 19:28:19 +0100

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# resc
+
+Regex escape helper. Reads stdin, escapes line by line.
+
+## Setup
+
+```
+go get github.com/thomasheller/resc
+```
+
+## Usage
+
+```
+$ resc
+Hello (World)?!
+Hello \(World\)\?!
+^D
+```
+
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		escaped := regexp.QuoteMeta(line)
+		fmt.Println(escaped)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("Error reading vom stdin: %v", err)
+	}
+}
